6d56c2ebafa277b91199176e1f6b3296c492ebf8
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-07 11:13:10 -0300

    Atuaizações gerais

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,23 @@ function App() {
   const [user, setUser] = useState();
 
   useEffect(() => {
+    
     api
       .get("/users/lucasgabriel2806")
       .then((response) => setUser(response.data))
       .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
 
-  useEffect(() => {
-   api
-     .post("https://minhaapi/novo-usuario",{
-        nome: 'Romulo',
-        sobrenome: 'Sousa'
-  })
-      .then((response) => setUser(response.data))
-      .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
 
+      });
+
+  }, []);
+  
   return (
 
     <div className="App">
 
-      {/*  */}
+      {/* O operador ?. (chamado optional chaining) impede erros caso user seja undefined. Sem isso, daria erro se tentasse acessar user.login antes da resposta da API.  */}
       <p>Usuário: {user?.login}</p>
 
       <p>Biográfia: {user?.bio}</p>
--- src/services/api.js
+++ src/services/api.js
@@ -6,15 +6,4 @@ const api = axios.create({
 
 });
 
-api.interceptors.request.use(async config => {
-  // Declaramos um token manualmente para teste.
-  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9";
-
-  if (token) {
-    api.defaults.headers.authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
 export default api;
